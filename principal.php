cef6710462acab675b64f13682f04bb50ab5a62f
abgarciar <informaticarevolucionaria@gmail.com>
2017-03-17 01:35:46 -0600

    borde y color

--- principal.php
+++ principal.php
@@ -126,19 +126,16 @@ else
 						<div class="section-heading text-right">
 					<!-- <h2 font color"White" class="h-ultra">Elección Niña Primavera y Niña Botoncito de Primavera 2017</h2>
 					<h4 class="h-light"> <i>Elección 2017</i></h4> -->
-					
-					<h2 font color"White" class="h-ultra"><i>Niña Primavera y Botoncito de Primavera</i></h2>
-					<h4 class="h-light"> <i>Elección 2017</i></h4>
+					<h3 font color"White" class="h-ultra"><i>Niña Primavera y Botoncito de Primavera</i></h3>
+					<h3 class="h-light"> <i>Elección 2017</i></h3>
 					<div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.1s">
 					</div>
 					</div>
 					<div class="col-lg-4">
 					<div class="col-lg-4">
-
 					</div>
 					</div>
-					
-					<div class="col-lg-6.5">
+					<div class="col-lg-6.0">
 						<div class="wow fadeInUp" data-wow-duration="2s" data-wow-delay="0.2s">
 						<div align= "absbottom right"><img src="img/dummy/img-1.png" class="img-responsive" alt="" />
 						</div>
